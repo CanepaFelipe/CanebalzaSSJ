acb4c3f36dab16d2a44b5a46d9e61111d6ef66ac
CanepaFelipe <luisfcanepa@gmail.com>
2024-09-25 11:08:34 -0300

    subo proyecto eclpi

---
+++ CanebalzaSSJ/src/test/java/com/CanebalzaSSJ/CanebalzaSSJ/CanebalzaSsjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CanebalzaSSJ.CanebalzaSSJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanebalzaSsjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
