ed3786dd71c6912f5bff7b21a4258751d06a5465
CanepaFelipe <luisfcanepa@gmail.com>
2024-09-26 13:52:19 -0300

    creo la bd

---
+++ CanebalzaSSJ/src/main/java/com/CanebalzaSSJ/CanebalzaSSJ/models/Turno.java
@@ -0,0 +1,84 @@
+package com.CanebalzaSSJ.CanebalzaSSJ.models;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "turnos")
+
+
+
+
+public class Turno{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private int idTurno;
+	private int idPaciente;
+	private int idMedico;
+	private String especialidad;
+	
+	@Column(columnDefinition = "TEXT")
+	private Date fecha;
+	private Time hora;
+	private String estado;
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdTurno() {
+		return idTurno;
+	}
+	public void setIdTurno(int idTurno) {
+		this.idTurno = idTurno;
+	}
+	public int getIdPaciente() {
+		return idPaciente;
+	}
+	public void setIdPaciente(int idPaciente) {
+		this.idPaciente = idPaciente;
+	}
+	public int getIdMedico() {
+		return idMedico;
+	}
+	public void setIdMedico(int idMedico) {
+		this.idMedico = idMedico;
+	}
+	public String getEspecialidad() {
+		return especialidad;
+	}
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Time getHora() {
+		return hora;
+	}
+	public void setHora(Time hora) {
+		this.hora = hora;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+}
+	
