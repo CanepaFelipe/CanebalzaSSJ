a01e1085c4739151a99a6b2a514325153ebf0d71
CanepaFelipe <luisfcanepa@gmail.com>
2024-09-26 14:15:43 -0300

    creo repository para permitir leer turnos en la db

---
+++ CanebalzaSSJ/src/main/java/com/CanebalzaSSJ/CanebalzaSSJ/services/TurnosRepository.java
@@ -0,0 +1,9 @@
+package com.CanebalzaSSJ.CanebalzaSSJ.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.CanebalzaSSJ.CanebalzaSSJ.models.Turno;
+
+public interface TurnosRepository extends JpaRepository<Turno, Integer>{
+
+}
